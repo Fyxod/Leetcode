59fb952bef06b9438420815e1e74c4ed2069e6bb
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2024-11-06 01:53:07 +0530

    Time: 0 ms (100%), Space: 42.8 MB (31.82%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations-bfs.java
@@ -0,0 +1,13 @@
+class Solution {
+    //an attempt to decrease the runtime(unsuccessful)
+    public int finalValueAfterOperations(String[] operations) {
+        int X = 0;
+        for (String s : operations) {
+            if (s.charAt(0) == '-' || s.charAt(1) == '-')
+                X--;
+            else
+                X++;
+        }
+        return X;
+    }
+}
