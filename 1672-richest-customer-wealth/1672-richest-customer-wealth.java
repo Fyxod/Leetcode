d3416c118669545e06c8931318d50639fb42e012
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2024-11-17 16:48:34 +0530

    Time: 0 ms (100%), Space: 42.6 MB (74.26%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -10,6 +10,8 @@ class Solution {
             }
             if(sum > wealth)
             wealth = sum;
+            if(sum == n*100) //more optimization
+            break;
         }
         return wealth;
     }
