c82b27eeb1b3536a4457b386b0a70ce10453aa23
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-03-21 14:39:55 +0530

    Time: 1 ms (70.95%), Space: 42.8 MB (8.53%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,18 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// Brute Force
+// class Solution {
+//     Map<Integer, TreeNode> map = new TreeMap<>();
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         if(root == null){
+//             return ans;
+//         }
+//         helper(0, root);
+//         for(TreeNode node: map.values()){
+//             ans.add(node.val);
+//         }
+//         return ans;
+//     }
+//     public void helper(int level, TreeNode root){
+//         if(root == null) return;
+//         map.putIfAbsent(level, root);
+//         if(root.right != null) helper(level + 1, root.right);
+//         if(root.left != null) helper(level + 1, root.left);
+//     }
+// }
+
+
 class Solution {
     Map<Integer, TreeNode> map = new TreeMap<>();
     public List<Integer> rightSideView(TreeNode root) {
@@ -20,16 +28,20 @@ class Solution {
         if(root == null){
             return ans;
         }
-        helper(0, root);
-        for(TreeNode node: map.values()){
-            ans.add(node.val);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode temp = q.poll();
+                if(i == size - 1){
+                    ans.add(temp.val);
+                }
+                if(temp.left != null) q.offer(temp.left);
+                if(temp.right != null) q.offer(temp.right);
+            }
         }
         return ans;
-    }
-    public void helper(int level, TreeNode root){
-        if(root == null) return;
-        map.putIfAbsent(level, root);
-        if(root.right != null) helper(level + 1, root.right);
-        if(root.left != null) helper(level + 1, root.left);
     }
 }
