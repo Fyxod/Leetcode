b6d3caf5bb0d72170a512859c3b9eb3eaa605167
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-03-24 14:40:08 +0530

    Time: 0 ms (100%), Space: 41.4 MB (76.44%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String ans = "";
+        int minIndex = 0;
+        int size = strs.length;
+        int minLength = strs[0].length();
+        for (int i = 1; i < size; i++) {
+            if (minLength > strs[i].length()) {
+                minLength = strs[i].length();
+                minIndex = i;
+            }
+        }
+        String check = strs[minIndex];
+        while(check.length() > 0){
+            boolean flag = true;
+            for(int j = 0; j < strs.length; j++){
+                if(j == minIndex) continue;
+                if(!(strs[j].startsWith(check))){
+                    flag = false;
+                    break;
+                }
+                
+            }
+            if(flag){
+                ans = check;
+                break;
+            }
+            check = check.substring(0, check.length() - 1);
+        }
+        return ans;
+    }
+}
