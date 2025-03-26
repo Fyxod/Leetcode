72980600441abe3891b15e98e549f91ece803614
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-03-26 23:02:13 +0530

    Time: 15 ms (57.11%), Space: 45.9 MB (29.14%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -2,23 +2,43 @@ class Solution {
     public String frequencySort(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
         StringBuilder ans = new StringBuilder();
-        int size = s.length();
-        for(int i = 0; i < size; i++){
-            char ch = s.charAt(i);
-            map.putIfAbsent(ch, 0);
-            map.put(ch, map.get(ch) + 1);
-        }
-        Map<Integer, ArrayList<Character>> freq = new TreeMap<>();
-        for(char a: map.keySet()){
-            freq.putIfAbsent(map.get(a), new ArrayList<Character>());
-            freq.get(map.get(a)).add(a);
+        for(char ch: s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
-        for(int a: freq.keySet()){
-            for(char ch: freq.get(a)){
-                ans.insert(0, String.valueOf(ch).repeat(a));
-            }
+        PriorityQueue<Map.Entry<Character, Integer>> heap = new PriorityQueue<>((a,b) -> b.getValue() - a.getValue());
+        heap.addAll(map.entrySet());
+
+        while(!heap.isEmpty()){
+            Map.Entry<Character, Integer> entry = heap.poll();
+            ans.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
         }
         return ans.toString();
     }
 }
+
+
+// class Solution {
+//     public String frequencySort(String s) {
+//         HashMap<Character, Integer> map = new HashMap<>();
+//         StringBuilder ans = new StringBuilder();
+//         int size = s.length();
+//         for(int i = 0; i < size; i++){
+//             char ch = s.charAt(i);
+//             map.putIfAbsent(ch, 0);
+//             map.put(ch, map.get(ch) + 1);
+//         }
+//         Map<Integer, ArrayList<Character>> freq = new TreeMap<>();
+//         for(char a: map.keySet()){
+//             freq.putIfAbsent(map.get(a), new ArrayList<Character>());
+//             freq.get(map.get(a)).add(a);
+//         }
+
+//         for(int a: freq.keySet()){
+//             for(char ch: freq.get(a)){
+//                 ans.insert(0, String.valueOf(ch).repeat(a));
+//             }
+//         }
+//         return ans.toString();
+//     }
+// }
