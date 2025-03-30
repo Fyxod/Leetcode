6dbf16ae6df7969671fad7bb2e32d58f05873527
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-03-31 01:40:36 +0530

    Time: 3 ms (23.01%), Space: 46.2 MB (19.19%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,7 +1,8 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int slow = 0;
-        for(int i = 0; i < nums.length; i++){
+        int size = nums.length;
+        for(int i = 0; i < size; i++){
             if(nums[i] != 0){
                 if(nums[i] != nums[slow]){
                     int temp = nums[slow];
