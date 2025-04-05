3f7770ff8bec259c0fdbca4dcd98c478b06200d0
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-04-05 13:20:03 +0530

    Time: 12 ms (35.59%), Space: 49.4 MB (89.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
-
+        int size = nums.length;
         for(int a : nums){
             map.put(a, map.getOrDefault(a, 0) + 1);
         }
@@ -14,7 +14,29 @@ class Solution {
                 max = entry.getValue();
                 ans = entry.getKey();
             }
+            if(max > size / 2) return ans;
         }
         return ans;
     }
 }
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         Map<Integer, Integer> map = new HashMap<>();
+
+//         for(int a : nums){
+//             map.put(a, map.getOrDefault(a, 0) + 1);
+//         }
+
+//         int max = 0;
+//         int ans = -1;
+
+//         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+//             if(entry.getValue() > max){
+//                 max = entry.getValue();
+//                 ans = entry.getKey();
+//             }
+//         }
+//         return ans;
+//     }
+// }
