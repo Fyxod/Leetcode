992238508fedc8fb0edd78df1f42f555807dbf5f
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-06-06 22:09:15 +0530

    Time: 2 ms (74.99%), Space: 53.2 MB (39.2%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,24 +1,127 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int size = nums.length;
+        int count = 0;
+        int temp = nums[0];
         for(int a : nums){
-            map.put(a, map.getOrDefault(a, 0) + 1);
-        }
-
-        int max = 0;
-        int ans = -1;
-
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            if(entry.getValue() > max){
-                max = entry.getValue();
-                ans = entry.getKey();
+            if(temp == a){
+                count++;
             }
-            if(max > size / 2) return ans;
+            else if(count == 0){
+                temp = a;
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count > nums.length / 2) return temp;
         }
-        return ans;
+        return temp;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int count = 0;
+//         int ans = nums[0];
+
+//         for(int a : nums){
+//             if(count == 0){
+//                 ans = a;
+//                 count++;
+//             }
+//             else if(ans == a){
+//                 count++;
+//             }
+//             else{
+//                 count--;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         int size = nums.length;
+//         for(int a : nums){
+//             map.put(a, map.getOrDefault(a, 0) + 1);
+//         }
+
+//         int max = 0;
+//         int ans = -1;
+
+//         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+//             if(entry.getValue() > max){
+//                 max = entry.getValue();
+//                 ans = entry.getKey();
+//             }
+//             if(max > size / 2) return ans;
+//         }
+//         return ans;
+//     }
+// }
 
 // class Solution {
 //     public int majorityElement(int[] nums) {
