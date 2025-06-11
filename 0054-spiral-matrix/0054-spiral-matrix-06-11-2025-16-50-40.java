8f40fa56efec3aa5e1c792ea4fff96e77483da9d
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-06-11 16:50:41 +0530

    Time: 0 ms (100%), Space: 41.7 MB (43.83%) - 06-11-2025

---
+++ 0054-spiral-matrix/0054-spiral-matrix-06-11-2025-16-50-40.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        spiral(matrix, m, n, 0, -1, 0, 1, result);
+        return result;
+    }
+
+    void spiral(int matrix[][], int m, int n, int r, int c, int dr, int dc, List<Integer> result){
+        if(m == 0 || n == 0) return;
+
+        for(int i = 0; i < n; i++){
+            r += dr;
+            c += dc;
+            result.add(matrix[r][c]);
+        }
+
+        spiral(matrix, n, m - 1, r, c, dc, -dr, result);
+    }
+}
