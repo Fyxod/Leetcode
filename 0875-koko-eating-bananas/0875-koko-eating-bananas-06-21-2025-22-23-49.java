b145d91c50d9c841c33dc8b2ab7d9913ab3221ef
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-06-21 22:23:49 +0530

    Time: 46 ms (5.96%), Space: 45.8 MB (5.49%) - 06-21-2025

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas-06-21-2025-22-23-49.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int max = piles[0];
+
+        for(int a : piles){
+            max = Math.max(max, a);
+        }
+        int high = max;
+        
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            int hours = getH(piles, mid);
+            if(hours <= h){
+                max = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return max;
+    }
+
+    int getH(int[] piles, int count){
+        int hours = 0;
+        for(int a : piles){
+            hours += Math.ceil((double)a / count);
+        }
+        System.out.println(count + " " + hours);
+        return hours;
+    }
+}
