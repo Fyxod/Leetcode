460a3f1fbb39d7c53bd068a5b9e47cbcf9482987
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-07-20 22:21:05 +0530

    Time: 11 ms (37.89%), Space: 57.2 MB (22.86%) - 07-20-2025

---
+++ 0148-sort-list/0148-sort-list-07-20-2025-22-21-04.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        return mergeSort(head);
+    }
+    ListNode mergeSort(ListNode head){
+        if(head == null || head.next == null) return head;
+        ListNode mid = findMid(head);
+        ListNode right = mergeSort(mid.next);
+        mid.next = null;
+        ListNode left = mergeSort(head);
+        return merge(left, right);
+    }
+    ListNode merge(ListNode left, ListNode right){
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+
+        while(left != null && right != null){
+            if(left.val > right.val){
+                temp.next = right;
+                right = right.next;
+            }
+            else{
+                temp.next = left;
+                left = left.next;
+            }
+            temp = temp.next;
+            if(right != null) temp.next = right;
+            else temp.next = left;
+        }
+
+        return dummy.next;
+    }
+    ListNode findMid(ListNode head){
+        ListNode slow = head, fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
