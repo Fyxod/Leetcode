641e89deda0cb1fd9777234a2f731d70d92c0f13
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-08-01 08:28:21 +0530

    Time: 1 ms (82.7%), Space: 43.5 MB (35.65%) - 08-01-2025

---
+++ 0022-generate-parentheses/0022-generate-parentheses-08-01-2025-08-28-20.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        helper(n, n*2, 0, 0, "", list);
+        return list;
+    }
+    void helper(int limit, int n, int start, int end, String curr, List<String> list){
+        if(n == 0){
+            list.add(curr);
+            return;
+        }
+        if(start == end){
+            helper(limit, n - 1, start + 1, end, curr + "(", list);
+        }
+        else if(start > end){
+            if(start < limit)helper(limit, n - 1, start + 1, end, curr + "(", list);
+            helper(limit, n - 1, start, end + 1, curr + ")", list);
+        }
+    }
+}
