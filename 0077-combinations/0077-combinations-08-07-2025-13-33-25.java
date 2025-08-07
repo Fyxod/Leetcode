043eee7c8992a61f53f483598db732fa2c297137
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-08-07 13:33:26 +0530

    Time: 18 ms (79.24%), Space: 94.7 MB (58.73%) - 08-07-2025

---
+++ 0077-combinations/0077-combinations-08-07-2025-13-33-25.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        boolean used[] = new boolean[n];
+        helper(n, k, ans, new ArrayList<>(), used, 1);
+        return ans;
+    }
+    void helper(int n, int k, List<List<Integer>> ans, List<Integer> curr, boolean used[], int start){
+        if(curr.size() == k){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int i = start; i <= n; i++){
+            if(!used[i - 1]){
+                curr.add(i);
+                used[i - 1] = true;
+                helper(n, k, ans, curr, used, i + 1);
+                curr.remove(curr.size() - 1);
+                used[i - 1] = false;
+            }
+        }
+    }
+}
