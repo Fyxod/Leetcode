59ca6426262e332b5596e48d3db0667ae19f09e9
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-08-23 15:43:58 +0530

    Time: 101 ms (40.54%), Space: 64.5 MB (7.7%) - 08-23-2025

---
+++ 0204-count-primes/0204-count-primes-08-23-2025-15-43-57.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countPrimes(int n) {
+        int primes[] = new int[n];
+        // for(int i = 0; i < n; i++){
+        //     primes[i] = 1;
+        // }
+        for(int i = 2; i * i < n; i++){
+            if(primes[i] == 0){
+                for(int j = i * i; j < n; j += i){
+                    primes[j] = 1;
+                }
+            }
+        }
+        int cnt = 0;
+        for(int i = 2; i < n; i++){
+            if(primes[i] == 0) cnt++;
+        }
+        return cnt;
+    }
+}
