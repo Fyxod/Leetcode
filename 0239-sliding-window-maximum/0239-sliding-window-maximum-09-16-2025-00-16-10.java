7506b6c5743a0bb5171c95708cafa974e00db952
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-09-16 00:16:11 +0530

    Time: 32 ms (51.29%), Space: 57.1 MB (98.84%) - 09-16-2025

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum-09-16-2025-00-16-10.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < k; i++){
+            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) dq.removeLast();
+            dq.addLast(i);
+        }
+        list.add(nums[dq.peekFirst()]);
+        for(int i = k; i < nums.length; i++){
+            if(!dq.isEmpty() && dq.peekFirst() == i - k) dq.removeFirst();
+            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) dq.removeLast();
+            dq.addLast(i);
+            list.add(nums[dq.peekFirst()]);
+        }
+        int ans[] = new int[list.size()];
+        for(int i = 0; i < ans.length; i++){
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+}
