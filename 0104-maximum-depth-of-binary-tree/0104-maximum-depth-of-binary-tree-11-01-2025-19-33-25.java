1eb7a5fb52f2177964062e7f5350b5a672994af8
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-11-01 19:33:26 +0530

    Time: 0 ms (100%), Space: 44.4 MB (5.77%) - 11-01-2025

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree-11-01-2025-19-33-25.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max = 0;
+    public int maxDepth(TreeNode root) {
+        helper(root, 1);
+        return max;
+    }
+    void helper(TreeNode root, int depth){
+        if(root == null) return;
+        max = Math.max(max, depth);
+        helper(root.left, depth + 1);
+        helper(root.right, depth + 1);
+    }
+}
