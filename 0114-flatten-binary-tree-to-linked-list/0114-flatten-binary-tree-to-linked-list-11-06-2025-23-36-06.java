d5c2e100a06dd6c734de931280edf1ad2d95047d
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-11-06 23:36:07 +0530

    Time: 1 ms (18.68%), Space: 44 MB (5.31%) - 11-06-2025

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list-11-06-2025-23-36-06.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        List<TreeNode> list = new ArrayList<>();
+        preorder(root, list);
+
+        for(int i = 0; i < list.size() - 1; i++){
+            TreeNode curr = list.get(i);
+            curr.left = null;
+            curr.right = list.get(i + 1);
+        }
+    }
+
+    void preorder(TreeNode root, List<TreeNode> list){
+        if(root == null) return;
+        list.add(root);
+
+        preorder(root.left, list);
+        preorder(root.right, list);
+    }
+}
