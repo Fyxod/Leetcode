6e6af034937796b0852658bab9d76db24effd88b
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-11-07 22:09:41 +0530

    Time: 1 ms (86.41%), Space: 47.3 MB (76.16%) - 11-07-2025

---
+++ 0547-number-of-provinces/0547-number-of-provinces-11-07-2025-22-09-39.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int prov = 0;
+        boolean vis[] = new boolean[isConnected.length];
+
+        for(int i = 0; i < isConnected.length; i++){
+            if(!vis[i]){
+                prov++;
+                dfs(i, vis, isConnected);
+            }
+        }
+        return prov;
+    }
+    void dfs(int node, boolean vis[], int[][] isConnected){
+        vis[node] = true;
+
+        for(int i = 0; i < isConnected[node].length; i++){
+            if(isConnected[node][i] == 1 && !vis[i]) dfs(i, vis, isConnected);
+        }
+    }
+}
