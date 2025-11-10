7b3eff1fb67eea7439e99e3380385b906cd3d9f8
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-11-10 22:22:41 +0530

    Time: 7 ms (4.15%), Space: 46 MB (6.7%) - 11-10-2025

---
+++ 0994-rotting-oranges/0994-rotting-oranges-11-10-2025-22-22-41.java
@@ -0,0 +1,57 @@
+class Solution {
+    class Pair{
+        int r;
+        int c;
+        int min = 0;
+        Pair(int r, int c, int min){
+            this.r = r;
+            this.c = c;
+            this.min  = min;
+        }
+    }
+
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        Set<String> set = new HashSet<>();
+        Queue<Pair> q = new ArrayDeque<>();
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 2) q.offer(new Pair(i, j, 0));
+            }
+        }
+
+        int minutes = 0;
+        while(!q.isEmpty()){
+            Pair curr = q.poll();
+            int r = curr.r;
+            int c = curr.c;
+            int min = curr.min;
+            minutes = Math.max(minutes, min);
+
+            set.add(r + "_" + c);
+            if(r > 0 && grid[r - 1][c] != 0 && grid[r - 1][c] != 2 && !set.contains((r - 1) + "_" + c)){
+                grid[r - 1][c] = 2;
+                q.offer(new Pair(r - 1, c, min + 1));
+            }
+            if(c > 0 && grid[r][c - 1] != 0 && grid[r][c - 1] != 2 && !set.contains(r + "_" + (c - 1))){
+                grid[r][c - 1] = 2;
+                q.offer(new Pair(r, c - 1, min + 1));
+            }
+            if(r < m - 1 && grid[r + 1][c] != 0 && grid[r + 1][c] != 2 && !set.contains((r + 1) + "_" + c)){
+                grid[r + 1][c] = 2;
+                q.offer(new Pair(r + 1, c, min + 1));
+            }
+            if(c < n - 1 && grid[r][c + 1] != 0 && grid[r][c + 1] != 2 && !set.contains(r + "_" + (c + 1))){
+                grid[r][c + 1] = 2;
+                q.offer(new Pair(r, c + 1, min + 1));
+            }
+        }
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1) return -1;
+            }
+        }
+        return minutes;
+    }
+}
