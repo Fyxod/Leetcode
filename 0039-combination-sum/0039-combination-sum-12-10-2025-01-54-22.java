5c1adc8e6d5d7eb249397c32224d4aa920443184
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-12-10 01:54:23 +0530

    Time: 2 ms (92.16%), Space: 45.5 MB (56.83%) - 12-10-2025

---
+++ 0039-combination-sum/0039-combination-sum-12-10-2025-01-54-22.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        helper(list, new ArrayList<>(), candidates, target, 0, 0);
+        return list;
+    }
+    void helper(List<List<Integer>> list, List<Integer> curr, int[] cand, int t, int start, int sum){
+        if(sum == t) {
+            list.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int i = start; i < cand.length; i++){
+            if(sum + cand[i] <= t){
+                curr.add(cand[i]);
+                helper(list, curr, cand, t, i, sum + cand[i]);
+                curr.remove(curr.size() - 1);
+            }
+        }
+    }
+}
