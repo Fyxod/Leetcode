7981378112d59d4dda9d9ac200a682973c78bcb4
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-12-26 16:05:09 +0530

    Time: 181 ms (30.73%), Space: 53.4 MB (6.73%) - 12-26-2025

---
+++ 0494-target-sum/0494-target-sum-12-26-2025-16-05-09.java
@@ -0,0 +1,22 @@
+class Solution {
+    record Pair(int i, int sum){};
+    static int INF = Integer.MAX_VALUE/2;
+    public int findTargetSumWays(int[] nums, int t) {
+        int n = nums.length;
+        int sum = 0;
+        for(int a : nums) sum += a;
+        Map<Pair, Integer> dp = new HashMap<>();
+        return f(nums, dp, t, 0, 0);
+    }
+    int f(int nums[], Map<Pair, Integer> dp, int t, int i, int sum){
+        if(i == nums.length){
+            if(t == sum) return 1;
+            else return 0;
+        }
+        Pair lookup = new Pair(i, sum);
+        if(dp.containsKey(lookup)) return dp.get(lookup);
+        int res = f(nums, dp, t, i + 1, sum + nums[i]) + f(nums, dp, t, i + 1, sum - nums[i]);
+        dp.put(new Pair(i, sum), res);
+        return res;
+    }
+}
