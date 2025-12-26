01b3970b491b4055c3ae573938ec761c11883989
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-12-27 00:27:48 +0530

    Time: 7 ms (72.46%), Space: 43 MB (80.63%) - 12-27-2025

---
+++ 0518-coin-change-ii/0518-coin-change-ii-12-27-2025-00-27-48.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        if(amount == 0) return 1;
+        int n = coins.length;
+        int dp[] = new int[amount + 1];
+        dp[0] = 1;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j <= amount; j++){
+                if(coins[i - 1] <= j) dp[j] += dp[j - coins[i - 1]];
+            }
+        }
+        return dp[amount];
+    }
+}
