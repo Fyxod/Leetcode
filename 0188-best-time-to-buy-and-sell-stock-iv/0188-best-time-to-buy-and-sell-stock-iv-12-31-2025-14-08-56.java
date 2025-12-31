1b42370067053dd5a532d11589f7782be8742643
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2025-12-31 14:08:57 +0530

    Time: 7 ms (42.56%), Space: 44.8 MB (59.34%) - 12-31-2025

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv-12-31-2025-14-08-56.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxProfit(int k, int[] p) {
+        int n = p.length;
+        if (k >= n / 2) {
+    int profit = 0;
+    for (int i = 1; i < n; i++) {
+        if (p[i] > p[i-1])
+            profit += p[i] - p[i-1];
+    }
+    return profit;
+}
+
+        int dp[][][] = new int[n][2][k];
+        for (int arr[][] : dp)
+            for(int arr2[] : arr) Arrays.fill(arr2, -1);
+        return f(p, dp, 0, 1, 0, k);
+    }
+
+    int f(int p[], int dp[][][], int i, int buy, int lim, int k){
+        if(i == p.length || lim == k) return 0;
+
+        if(dp[i][buy][lim] != -1) return dp[i][buy][lim];
+
+        if(buy == 1) return dp[i][buy][lim] = Math.max(
+            -p[i] + f(p, dp, i + 1, 0, lim, k),
+            f(p, dp, i + 1, 1, lim, k)
+        );
+        else return dp[i][buy][lim] = Math.max(
+            p[i] + f(p, dp, i + 1, 1, lim + 1, k),
+            f(p, dp, i + 1, 0, lim, k)
+        );
+    }
+}
