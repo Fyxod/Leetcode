b33bd0649f01a93f3fd48550ad07f40dd710d737
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2026-01-13 19:27:36 +0530

    Time: 0 ms (100%), Space: 42.4 MB (7.88%) - 01-13-2026

---
+++ 0070-climbing-stairs/0070-climbing-stairs-01-13-2026-19-27-35.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        return f(dp, n);
+    }
+    int f(int dp[], int n){
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = f(dp,n - 1) + f(dp, n - 2);
+    }
+}
