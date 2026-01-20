144dbfb05977203a1bb011a8e6404983ed68bae0
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2026-01-20 13:35:26 +0530

    Time: 23 ms (70.74%), Space: 42.7 MB (99.54%) - 01-20-2026

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome-01-20-2026-13-35-25.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minInsertions(String s) {
+        String t = new StringBuilder(s).reverse().toString();
+        return s.length() - f(s, t);
+    }
+    public int f(String text1, String text2) {
+        int n1 = text1.length();
+        int n2 = text2.length();
+
+        int dp[] = new int[n2 + 1];
+        int curr[] = new int[n2 + 1];
+        for(int i = n1 - 1; i >= 0; i--){
+            for(int j = n2 - 1; j >= 0; j--){
+                if(text1.charAt(i) == text2.charAt(j)){
+                    curr[j] = 1 + dp[j + 1];
+                }
+                else curr[j] = Math.max(dp[j], curr[j + 1]);
+            }
+            int temp[] = dp;
+            dp = curr;
+            curr = temp;
+        }
+
+        return dp[0];
+    }
+}
