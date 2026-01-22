cc0bd6b473d58e9fc6640235fef13c62ad56864f
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2026-01-22 23:21:11 +0530

    Time: 919 ms (10.02%), Space: 109.5 MB (25.58%) - 01-22-2026

---
+++ 0815-bus-routes/0815-bus-routes-01-22-2026-23-21-10.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int numBusesToDestination(int[][] routes, int src, int target) {
+        if(src == target) return 0;
+        int n = routes.length;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < n; i++){
+            for(int node : routes[i]){
+                map.computeIfAbsent(node, k -> new ArrayList<>()).add(i);
+            }
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+        if(!map.containsKey(src)) return -1;
+        for(int bus : map.get(src)){
+            q.offer(bus);
+            set.add(bus);
+        }
+
+        int buses = 1;
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+
+            while(size-- > 0){
+                int curr = q.poll();
+
+                for(int nei : routes[curr]){
+                    if(nei == target) return buses;
+                    for(int bus : map.get(nei)){
+                        if(set.add(bus)) q.offer(bus);
+                    }
+                }
+            }
+            buses++;
+        }
+
+        return -1;
+    }
+}
