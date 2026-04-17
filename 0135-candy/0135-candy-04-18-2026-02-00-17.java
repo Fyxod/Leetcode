25360aaa3db45ef1ca770157ca1987d864b7e214
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2026-04-18 02:00:18 +0530

    Time: 3 ms (87.28%), Space: 47.6 MB (55.93%) - 04-18-2026

---
+++ 0135-candy/0135-candy-04-18-2026-02-00-17.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int candy(int[] rating) {
+        int n = rating.length;
+        
+        int cnt = 1;
+        int i = 1;
+        while(i < n){
+            if(rating[i] == rating[i - 1]){
+                i++;
+                cnt++;
+                continue;
+            }
+            int peak = 1;
+            while(i < n && rating[i] > rating[i - 1]){
+                peak++;
+                cnt += peak;
+                i++;
+            }
+            int down = 1;
+            while(i < n && rating[i] < rating[i - 1]){
+                cnt += down;
+                down++;
+                i++;
+            }
+            if(down > peak) cnt += down - peak;
+        }
+
+        return cnt;
+    }
+}
