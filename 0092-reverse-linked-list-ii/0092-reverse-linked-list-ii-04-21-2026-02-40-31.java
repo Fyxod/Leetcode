e064beae22ee4c785c2e3cef680ca7dc9cb06abe
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2026-04-21 02:40:32 +0530

    Time: N/A (0%), Space: N/A (0%) - 04-21-2026

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii-04-21-2026-02-40-31.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head == null || left == right) return head;
+        ListNode prev = null;
+        ListNode curr = head;
+        int pos = 1;
+        while(pos != left){
+            prev = curr;
+            curr = curr.next;
+            pos++;
+        }
+        ListNode leftNode = curr;
+        while(pos != right){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            pos++;
+        }
+        if(leftNode.next != null){
+            leftNode.next.next = curr;
+        }
+        leftNode.next = curr.next;
+        curr.next = prev;
+        if(left == 1) return curr;
+        return head;
+    }
+}
+
+
+
+// I am so fucking dumb
+// class Solution {
+//     public ListNode reverseBetween(ListNode head, int left, int right) {
+//         ListNode prev = null;
+//         ListNode curr = head;
+//         int pos = 1;
+//         while(pos != left){
+//             prev = curr;
+//             curr = curr.next;
+//             pos++;
+//         }
+//         ListNode leftNode = curr;
+//         while(pos != right){
+//             ListNode next = curr.next;
+//             curr.next = prev;
+//             prev = curr;
+//             curr = next;
+//             pos++;
+//         }
+//         if(left != right && leftNode.next != null){
+//             leftNode.next.next = curr;
+//         }
+//         leftNode.next = curr.next;
+//         if(left != right) curr.next = prev;
+//         if(left == 1) return curr;
+//         return head;
+//     }
+// }
