9a35ad8771321e248f8f60dced20e14b7975bc60
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2026-04-22 13:46:24 +0530

    Time: N/A (0%), Space: N/A (0%) - 04-22-2026

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary-04-22-2026-13-46-23.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> twoEditWords(String[] queries, String[] dict) {
+        int length = queries[0].length();
+        List<String> ans = new ArrayList<>();
+
+        for(String a : queries){
+            for(String b : dict){
+                int diff = 0;
+                for(int i = 0; i < length; i++){
+                    if(a.charAt(i) != b.charAt(i)){
+                        diff++;
+                        if(diff > 2) break;
+                    }
+                }
+                if(diff <= 2){
+                    ans.add(a);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
