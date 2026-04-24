af7bfbf40d790a59a80afaa3f6b381f8d8280d29
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2026-04-24 18:25:02 +0530

    Time: N/A (0%), Space: N/A (0%) - 04-24-2026

---
+++ 0148-sort-list/0148-sort-list-04-24-2026-18-25-00.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode slow = head, fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode mid = slow;
+        ListNode sorted1 = sortList(mid.next);
+        mid.next = null;
+        ListNode sorted2 = sortList(head);
+
+        return merge(sorted1, sorted2);
+    }
+    ListNode merge(ListNode head1, ListNode head2){
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        while(head1 != null && head2 != null){
+            if(head1.val < head2.val){
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            }
+            else{
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+
+        if(head1 != null){
+            temp.next = head1;
+        }
+        if(head2 != null){
+            temp.next = head2;
+        }
+
+        return dummy.next;
+    }
+}
