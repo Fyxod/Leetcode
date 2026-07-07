41068d33435758086c3465a488aeb9e9cf798922
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2026-07-07 20:43:25 +0530

    Time: 30 ms (13.56%), Space: 57 MB (34.5%) - 07-07-2026

---
+++ 0518-coin-change-ii/0518-coin-change-ii-07-07-2026-20-43-23.java
@@ -0,0 +1,28 @@
+class Solution {
+    int coins[];
+    int amount;
+    int n;
+    int dp[][];
+    public int change(int amount, int[] coins) {
+        if(amount == 0) return 1;
+        this.amount = amount;
+        this.coins = coins;
+        n = coins.length;
+
+        dp = new int[n][amount + 1];
+        for(int arr[] : dp) Arrays.fill(arr, -1);
+
+        return f(0, amount);
+    }
+    int f(int i, int sum){
+        if(sum == 0)  return 1;
+        if(i == n || sum < 0) return 0;
+
+        if(dp[i][sum] != -1) return dp[i][sum];
+
+        dp[i][sum] = f(i + 1, sum);
+        if(sum - coins[i] >= 0) dp[i][sum] += f(i, sum - coins[i]);
+
+        return dp[i][sum];
+    }
+}
