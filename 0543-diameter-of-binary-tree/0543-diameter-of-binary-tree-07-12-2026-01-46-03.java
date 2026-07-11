475c2dfbcbb75a043301591c0fe46002ffc28f37
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2026-07-12 01:46:04 +0530

    Time: 1 ms (46.37%), Space: 46.2 MB (99.85%) - 07-12-2026

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree-07-12-2026-01-46-03.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        f(root);
+        return max;
+    }
+    int f(TreeNode root){
+        if(root == null) return 0;
+        int sum = 0;
+        int left = f(root.left);
+        int right = f(root.right);
+
+        max = Math.max(max, left + right);
+        return 1 + Math.max(left, right);
+    }
+}
