7f5e18148ed27cf0cb6e0052735262b2a0485099
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2026-07-12 18:01:30 +0530

    Time: 1 ms (15.07%), Space: 45.6 MB (5.11%) - 07-12-2026

---
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras-07-12-2026-18-01-29.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// HAS_CAMERA = 0;
+// COVERED = 1
+// NEED_CAMERA = 2
+class Solution {
+    int INF = 100000000;
+    Map<TreeNode, int[]> map = new HashMap<>();
+    public int minCameraCover(TreeNode root) {
+        int res[] = f(root);
+        return Math.min(res[0], res[1]);
+    }
+    int[] f(TreeNode root){
+        if(root == null) return new int[]{INF, 0, INF};
+        
+        int res[] = new int[3];
+        int left[] = f(root.left);
+        int right[] = f(root.right);
+        
+        int min = INF;
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                min = Math.min(min, left[i] + right[j]);;
+            }
+        }
+        res[0] = 1 + min;
+        int case1 = left[0] + Math.min(right[0], right[1]);
+        int case2 = right[0] + Math.min(left[0], left[1]);
+        int case3 = left[0] + right[0];
+        res[1] = Math.min(case1, Math.min(case2, case3));
+        res[2] = left[1] + right[1];
+
+        return res;
+    } 
+}
