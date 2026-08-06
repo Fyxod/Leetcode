fdb470b351d3a31f8fa753233993b28e50ee38d2
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2026-08-06 21:42:50 +0530

    Time: 28 ms (88.69%), Space: 78.4 MB (29%) - 08-06-2026

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram-08-06-2026-21-42-49.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int ns[] = new int[n];
+        int ps[] = new int[n];
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i = 0; i < n; i++){
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]) stack.pop();
+            ps[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        stack.clear();
+        for(int i = n - 1; i >= 0; i--){
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]) stack.pop();
+            ns[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            max = Math.max(max, (ns[i] - ps[i] - 1) * heights[i]);
+        }
+
+        return max;
+    }
+}
