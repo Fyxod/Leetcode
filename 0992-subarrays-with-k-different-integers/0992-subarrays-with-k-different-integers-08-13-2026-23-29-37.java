17976b6d40771266d56d5d50a053bbf1e0cd3a4a
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2026-08-13 23:29:38 +0530

    Time: 6 ms (93.99%), Space: 49.1 MB (89.49%) - 08-13-2026

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers-08-13-2026-23-29-37.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return f(nums, k) - f(nums, k - 1);
+    }
+    int f(int nums[], int k){
+        if(k == 0) return 0;
+        int n = nums.length;
+        int freq[] = new int[20000 + 1];
+        int ans = 0;
+        int l = 0;
+        int cnt = 0;
+
+        for(int r = 0; r < n; r++){
+            int el = nums[r];
+            freq[el]++;
+            if(freq[el] == 1) cnt++;
+
+            while(cnt > k){
+                int temp = nums[l];
+                freq[temp]--;
+                if(freq[temp] == 0) cnt--;
+                l++;
+            }
+            ans += r - l + 1;
+        }
+
+        return ans;
+
+    }
+}
