64a864bf21fd6a57920b93f8bbe6506fe993eb62
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2026-08-30 00:55:48 +0530

    Time: 2 ms (99.34%), Space: 47.2 MB (23.75%) - 08-30-2026

---
+++ 0001-two-sum/0001-two-sum-08-30-2026-00-55-47.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int ans[] = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int a = nums[i];
+            if(map.containsKey(target - a)){
+                ans[0] = i;
+                ans[1] = map.get(target - a);
+                break;
+            }
+            map.put(a, i);
+        }
+
+        return ans;
+    }
+}
