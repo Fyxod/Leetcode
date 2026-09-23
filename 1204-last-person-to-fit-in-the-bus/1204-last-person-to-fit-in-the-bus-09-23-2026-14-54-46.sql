6c6fa48b57e9401b111186d1f2596e0dcfe791f9
Parth Katiyar <140262636+Fyxod@users.noreply.github.com>
2026-09-23 14:54:47 +0530

    Time: 712 ms (99.21%), Space: 0B (100%) - 09-23-2026

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus-09-23-2026-14-54-46.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+With q AS (
+    Select *,
+    SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM Queue
+)
+Select person_name
+FROM q
+where total_weight <= 1000
+GROUP BY person_id
+ORDER BY turn DESC
+limit 1
